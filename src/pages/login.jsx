858da2f59ec5f463434d0e63fd1cc65b97257247
alanglei <alan@consultesis.com.br>
2026-08-17 14:43:28 -0300

    ajuste no nome do arquivo css do login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import { useAuth } from "../contexts/AuthContext";
-import "./Login.css";
+import "./login.css";
 
 function Login() {
   const { login } = useAuth();  
